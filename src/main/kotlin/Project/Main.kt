b5b08b55a82d6990796bf35632789705b6dc269e
AndoDzhan1 <dzansuandrej@gmail.com>
2025-09-21 15:00:00 +0300

    KTB-04

--- src/main/kotlin/Project/Main.kt
+++ src/main/kotlin/Project/Main.kt
@@ -4,12 +4,31 @@ import java.io.File
 
 fun main() {
 
-    val wordsFile: File = File ("words.txt")
+    val wordsFile: File = File("words.txt")
+
+    val dictionary: MutableList<Word> = mutableListOf()
 
     wordsFile.createNewFile()
-    wordsFile.writeText("Hello - Привет")
-    wordsFile.appendText("Dog - Собака")
-    wordsFile.appendText("Cat - Кошка")
+    wordsFile.writeText("Hello|Привет|2")
+    wordsFile.appendText("\n")
+    wordsFile.appendText("Dog|Собака")
+    wordsFile.appendText("\n")
+    wordsFile.appendText("Cat|Кошка|5")
 
-    wordsFile.readLines().forEach { println(it) }
+    val lines: List<String> = wordsFile.readLines()
+    for (line in lines) {
+        val parts = line.split("|")
+
+        val count = parts.getOrNull(2)?.toIntOrNull() ?: 0
+
+        val word = Word(original = parts[0], translate = parts[1], correctAnswersCount = count)
+        dictionary.add(word)
+    }
+    println(dictionary)
 }
+
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
